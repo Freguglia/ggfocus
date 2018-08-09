bf29b28507e02baa9a549fedfa6daa5d7409da8d
Victor <victorfreguglia@gmail.com>
2018-08-09 09:54:10 -0300

    scale_alpha_focus() completed

--- R/scale_alpha_focus.R
+++ R/scale_alpha_focus.R
@@ -1,9 +1,27 @@
+#'  scale_color_focus
+#'
+#' @description    Alpha scale that focus specific factor levels. This is an alternative use of \link{ggfocus} function that uses the usual grammar of graphics.
+#'
+#' @param focus_levels character vector with levels to focus.
+#' @param alpha_focus `alpha` for focused levels.
+#' @param alpha_other `alpha` for other levels.
+#'
+#' @example
+#'  p <- ggplot(iris,aes(x = Petal.Length, y = Sepal.Length, alpha = Species)) + geom_point()
+#'  # Note this gives a warning because alpha doesn't always make sense with categorical variables.
+#'
+#'  p + scale_alpha_focus(focus_levels = "versicolor")
+#'  ## This is the same as
+#'  ggfocus(p, Species, focus_levels = "versicolor", focus_aes = "alpha")
+#'
+#' @export
 scale_alpha_focus <- function(focus_levels,alpha_focus=1,alpha_other=0.05){
   structure(list(focus_levels = focus_levels,
                  alpha_focus = alpha_focus,
                  alpha_other = alpha_other),class="ggfocus_alpha")
 }
 
+#' @export
 ggplot_add.ggfocus_alpha <- function(object, plot, objectname){
   p1 <- plot
   focus_levels <- object$focus_levels

6ea3d18f2b1aba48744894480022afab43666fbe
Freguglia <victorfreguglia@gmail.com>
2019-12-16 16:21:36 -0300

    Added separate file for handling outer functions.

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+#' @importFrom magrittr %>%
+#' @importFrom ggplot2 aes scale_alpha_manual scale_color_manual scale_fill_manual
+#' @export
+magrittr::'%>%'
+
+#' @importFrom ggplot2 ggplot_add
+#' @export
+ggplot_add

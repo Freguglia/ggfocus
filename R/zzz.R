5fc9f97b391b62b3890290dbb873765de752b1db
Freguglia <victorfreguglia@gmail.com>
2019-12-16 16:27:57 -0300

    Moved all imports to zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,8 @@
-#' @importFrom magrittr %>%
+#' @importFrom dplyr select_
 #' @importFrom ggplot2 aes scale_alpha_manual scale_color_manual scale_fill_manual
+#' @importFrom magrittr %>%
+NULL
+
 #' @export
 magrittr::'%>%'
 

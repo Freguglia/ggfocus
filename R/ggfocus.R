c7cc88437c6dce7c40545e0f7a4bea06b24209c0
Freguglia <victorfreguglia@gmail.com>
2019-12-16 16:36:59 -0300

    Deprecated ggfocus() function.

--- R/ggfocus.R
+++ R/ggfocus.R
@@ -7,7 +7,6 @@
 #'
 #' Creates a `ggplot` object with focus scales from another `ggplot` object.
 #'
-#'
 #' @param p a `ggplot` object.
 #' @param var Sets.
 #' @param focus_levels levels to be highlited.
@@ -15,29 +14,28 @@
 #' @param color_focus vector of colors (or a single color) for focused levels.
 #' @param color_other color for non-focused levels.
 #' @param alpha_focus,alpha_other alpha value for focused and non-focused levels.
+#'
 #' @return a ggplot object with focusing scales.
-#' @importFrom magrittr %>%
-#' @importFrom dplyr select
-#' @importFrom rlang enquo !!
-#' @author Victor Freguglia
+#'
 #' @examples
 #' library(ggplot2)
 #' p <- ggplot(iris,aes(x=Sepal.Length,y=Petal.Length)) + geom_point()
 #' ggfocus(p, Species, "versicolor")
 #'
-
+#' @author Victor Freguglia
+#' @export
 ggfocus <- function(p,
-                     var, focus_levels,
-                     focus_aes=c("color","alpha","fill"),
-                     color_focus = NULL, color_other = "black",
-                     alpha_focus = 1, alpha_other = 0.05){
+                    var, focus_levels,
+                    focus_aes=c("color","alpha","fill"),
+                    color_focus = NULL, color_other = "black",
+                    alpha_focus = 1, alpha_other = 0.05){
 
-  message("The function ggfocus() is deprecated, consider using the family scale_*_focus() instead.")
+  message("The function 'ggfocus()' is deprecated, consider using the family scale_*_focus() instead.")
 
   p1 <- p
   data <- p$data
   var <- enquo(var)
-  var_column <- data %>% select(!!var) %>% lapply(as.character) %>% unlist
+  var_column <- data %>% select_(var) %>% lapply(as.character) %>% unlist
   if("Other" %in% focus_levels){stop("'Other' cannot be the name of a focus level.")}
 
   if(".marker" %in% colnames(data)){data$.marker=NULL}

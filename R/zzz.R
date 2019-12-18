64c065587ab820bd6d069391ae060992c568ba91
Freguglia <victorfreguglia@gmail.com>
2019-12-17 22:15:20 -0300

    Importing mutate from dplyr.

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-#' @importFrom dplyr select_
+#' @importFrom dplyr select_ mutate
 #' @importFrom ggplot2 aes scale_alpha_manual scale_color_manual scale_fill_manual
 #' @importFrom rlang enquo
 #' @importFrom magrittr %>%

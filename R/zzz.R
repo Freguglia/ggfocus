e65918d115668ff4f4fbe3c2ecfb8dca91d53b7a
Freguglia <victorfreguglia@gmail.com>
2019-12-20 18:53:34 -0300

    Resolved NAMESPACE note on CHECK.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 #' @importFrom dplyr select_ mutate
-#' @importFrom ggplot2 aes scale_alpha_manual scale_color_manual scale_fill_manual scale_size_manual
+#' @importFrom ggplot2 aes scale_alpha_manual scale_color_manual scale_fill_manual scale_size_manual scale_shape_manual
 #' @importFrom rlang enquo
 #' @importFrom magrittr %>%
 NULL
@@ -16,4 +16,9 @@ ggplot_add
 #' @usage NULL
 scale_colour_focus <- scale_color_focus
 
-globalVariables(c(".marker_color", ".marker_fill", ".marker_alpha", ".marker_size"))
+globalVariables(c(".marker_color",
+                  ".marker_fill",
+                  ".marker_alpha",
+                  ".marker_size",
+                  ".marker_shape",
+                  ".marker_linetype"))
